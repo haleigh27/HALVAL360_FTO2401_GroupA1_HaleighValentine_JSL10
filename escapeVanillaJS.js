d72d79c4fdbff8d3adc01b0f0be26671b3b3dd3d
Haleigh <haleighvalentine@gmail.com>
2024-04-19 09:43:08 +0200

    Update comments and add error alerts

--- escapeVanillaJS.js
+++ escapeVanillaJS.js
@@ -1,55 +1,61 @@
 document.addEventListener('DOMContentLoaded', () => {
-    // 🪲 Bug: Incorrect ID used for attaching the event listener
+    // Click event listener: fetches books array of objects from books.json, calls findMostRecentBook() and displays it.
     document.getElementById('solveRoom1').addEventListener('click', () => {
         fetch('books.json')
             .then((response) => response.json())
             .then((books) => {
                 const mostRecentBook = findMostRecentBook(books);
-                // 🪲 Bug: Incorrect element ID
+
                 document.getElementById(
                     'room1Result'
                 ).textContent = `The key to the next room is: ${mostRecentBook.title}`;
+            })
+            .catch((err) => {
+                alert(`Error: ${err.message}`);
+                console.log(err);
             });
     });
 
+    // Click event listener: declares jsConcepts and reactConcepts, calls findIntersection() and displays it.
     document.getElementById('solveRoom2').addEventListener('click', () => {
         const jsConcepts = new Set(['closure', 'scope', 'hoisting', 'async']);
-        // 🪲 Bug: What's missing from JS concepts?
         const reactConcepts = new Set(['components', 'jsx', 'hooks', 'async']);
-        // 🪲 Bug: Incorrect function call
+
         const commonConcepts = findIntersection(jsConcepts, reactConcepts);
         document.getElementById('room2Result').textContent = `The code to unlock the door is: ${Array.from(
             commonConcepts
         ).join(', ')}`;
     });
 
-    // 🪲 Bug: Asynchronous function ?
+    // Click event listener with asynchronous function: fetches an array of objects from directions.json, calls navigateLabyrinth() and displays the message returned.
     document.getElementById('solveRoom3').addEventListener('click', async () => {
         try {
             const response = await fetch('directions.json');
             const directions = await response.json();
             const message = await navigateLabyrinth(directions);
-            // 🪲 Bug: Incorrect method
+
             document.getElementById('room3Result').textContent = message;
         } catch (err) {
-            console.log('Error:', err);
+            alert(`Error: ${err.message}`);
+            console.log(err);
         }
     });
 });
 
+//Function compares and finds most recently published book
 function findMostRecentBook(books) {
-    // 🪲 Bug: Logic error
     return books.reduce((mostRecent, book) => {
         return new Date(book.published) > new Date(mostRecent.published) ? book : mostRecent;
     });
 }
 
+//Function compares two arrays of data and returns an array of common values.
 function findIntersection(setA, setB) {
-    // 🪲 Bug: Incorrect logic
     const intersection = new Set([...setA].filter((concept) => setB.has(concept)));
     return intersection;
 }
 
+//Asynchronous function which loops through an array, logs the values in the console and then returns a message.
 async function navigateLabyrinth(directions) {
     for (let direction of directions) {
         // 🪲 Bug: No delay
